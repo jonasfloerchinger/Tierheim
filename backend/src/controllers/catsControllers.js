40255be99270e7c6a3619e4b87253f234992c54b
LucaBaschek <113526814+LucaBaschek@users.noreply.github.com>
2023-03-04 18:23:33 +0100

    CORS im Backend

--- backend/src/controllers/catsControllers.js
+++ backend/src/controllers/catsControllers.js
@@ -3,6 +3,7 @@ import { Cat } from "../models/cat.js";
 
 
 export const getCat=async (req,res)=> {
+    res.set("Access-Control-Allow-Origin", "http://localhost:4000");
     const cats = await Cat.find();
     res.status(200).send(cats);
 };
--- backend/src/controllers/hamstersControllers.js
+++ backend/src/controllers/hamstersControllers.js
@@ -3,6 +3,7 @@ import { Hams } from "../models/hamster.js";
 
 
 export const getHams=async (req,res)=> {
+    res.set("Access-Control-Allow-Origin", "http://localhost:4000");
     const hams = await Hams.find();
     res.status(200).send(hams);
 };
